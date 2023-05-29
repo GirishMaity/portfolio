ef61a5f10e2d553cf7ca1131e20f54c7fcad79ce
Girish Maity <girishmaity164@gmail.com>
2023-05-29 21:48:05 +0530

    minor updates

--- src/components/Homepage/Homepage.js
+++ src/components/Homepage/Homepage.js
@@ -10,47 +10,45 @@ import home from "../../Assets/animations/home.json";
 function Home() {
   return (
     <div className="homepagebackground">
-      <Container>
-        <Row>
-          <Col md={7}>
-            <h2 className="headtext">
-              Hello <span className="wave">👋 </span>
-            </h2>
-            <h2 className="nametext">I'm Girish Maity</h2>
-            <span></span>
-            <Text />
-            <button
-              onClick={() => {
-                window.open("https://github.com/GirishMaity");
-              }}
-              className="socailmediabtn"
-            >
-              <AiFillGithub className="icon" />
-            </button>
-            <button
-              onClick={() => {
-                window.open("https://www.linkedin.com/in/girishmaity/");
-              }}
-              className="socailmediabtn"
-            >
-              <FaLinkedinIn className="icon" />
-            </button>
-          </Col>
+      <Container style={{ display: "flex", paddingTop: "6em" }}>
+        <Col md={7}>
+          <h2 className="headtext">
+            Hello <span className="wave">👋 </span>
+          </h2>
+          <h2 className="nametext">I'm Girish Maity</h2>
+          <span></span>
+          <Text />
+          <button
+            onClick={() => {
+              window.open("https://github.com/GirishMaity");
+            }}
+            className="socailmediabtn"
+          >
+            <AiFillGithub className="icon" />
+          </button>
+          <button
+            onClick={() => {
+              window.open("https://www.linkedin.com/in/girishmaity/");
+            }}
+            className="socailmediabtn"
+          >
+            <FaLinkedinIn className="icon" />
+          </button>
+        </Col>
 
-          <Col md={5}>
-            <div>
-              <Player
-                src={home}
-                background="transparent"
-                speed="1"
-                style={{ width: "400px", height: "400px" }}
-                loop
-                controls
-                autoplay
-              />
-            </div>
-          </Col>
-        </Row>
+        <Col md={5}>
+          <div>
+            <Player
+              src={home}
+              background="transparent"
+              speed="1"
+              style={{ width: "400px", height: "400px" }}
+              loop
+              controls
+              autoplay
+            />
+          </div>
+        </Col>
       </Container>
     </div>
   );
